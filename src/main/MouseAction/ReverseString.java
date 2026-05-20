4b7bf3e20a105b4bbaefa1e6019b6c7b9bc73121
shreyaumbarkar <shreyaumbarkar2307@gmail.com>
2026-05-20 23:24:55 +0530

    Added the reverse string program

---
+++ src/main/MouseAction/ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String name = "Shreya";
+        String reversed = "";
+
+        for(int i = name.length() - 1; i >= 0; i--) {
+            reversed = reversed + name.charAt(i);
+        }
+
+        System.out.println("Original String: " + name);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
